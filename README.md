9adadc92cf1746f36c1a03adef745a1a564d6e6b
lfergon <lfergon@gmail.com>
2015-02-15 18:21:20 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Review Backbone.js app
+
+Bundling:
+
+    $>browserify -r ./app/main.js:app > static/bundle.js
---
+++ app/models/movie.js
@@ -0,0 +1,1 @@
+var Backbone = require("Backbone");var Movie = Backbone.Model.extend({  defaults: {    title: "default",    year: 0,    description: "empty",    selected: false  }});module.exports = Movie;
